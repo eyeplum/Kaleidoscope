d5ac56851a2c4667900282bbb1020a8ea9e657e0
Yan Li <yan.li@ef.com>
2016-05-09 14:49:59 +0800

    Add a simple lexer

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <cctype>
+#include <cstdlib>
 
 enum Token {
   tok_eof = -1,
@@ -14,12 +15,71 @@ enum Token {
 static std::string IdentifierStr;
 static double NumVal;
 
+// Is number component
+static bool isnumcomp(char character) {
+  return isdigit(character) || character == '.';
+}
+
+// Get token from stdin
 static int gettok() {
   static char LastChar = ' ';
   
   // Skip whitespaces
   while (isspace(LastChar)) {
     LastChar = getchar();
+  }
+
+  // Handle alphabetic chars
+  if (isalpha(LastChar)) {
+    IdentifierStr = LastChar;
+
+    // Accumulate token string
+    while (isalnum((LastChar = getchar()))) {
+      IdentifierStr += LastChar;
+    }
+
+    // Return token type
+    if (IdentifierStr == "def") {
+      return tok_def;
+    }
+    if (IdentifierStr == "extern") {
+      return tok_extern;
+    }
+    
+    return tok_identifier;
+  }
+
+  // Handle numbers
+  if (isnumcomp(LastChar)) {
+    std::string numberString;
+    
+    do {
+      numberString += LastChar;
+      LastChar = getchar();
+    } while (isnumcomp(LastChar));
+
+    NumVal = strtod(numberString.c_str(), 0);
+    return tok_number;
+  }
+
+  // Handle comments
+  if (LastChar == '#') {
+    do {
+      LastChar = getchar();
+    } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF) {
+      gettok();
+    }
+  }
+
+  // Handle EOF
+  if (LastChar == EOF) {
+    return tok_eof;
+  } else {
+    char result = LastChar;
+    LastChar = getchar();
+    return result;
   }
 }
 
