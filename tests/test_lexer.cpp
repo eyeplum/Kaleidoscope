d61f6e0bd13f11ddb1267a4525e1e771491ad06b
Yan Li <yan.li@ef.com>
2016-05-11 14:26:05 +0800

    Test lexer

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -1,7 +1,30 @@
 #include "../src/lexer.h"
 
 int main(int argc, char *argv[]) {
-  gettok(); 
+  printf("=== Kaleidoscope Lexer Tests ===\n");
+  while (true) {
+    int type = gettok();
+    switch(type) {
+      case tok_eof:
+        printf("EOF\n");
+        return 0;
+      case tok_def:
+        printf("keyword: define\n");
+        break;
+      case tok_extern:
+        printf("keyword: extern\n");
+        break;
+      case tok_identifier:
+        printf("identifier: %s\n", IdentifierStr.c_str());
+        break;
+      case tok_number:
+        printf("number: %lf\n", NumVal);
+        break;
+      default:
+        printf("unknown: %c\n", type);
+        break;
+    }
+  }
   return 0;
 }
 
